4215c75e6b2f2a473872a07f82d4457b7fc2f620
StraniK3005 <StraniK.30.05@gmail.com>
2019-11-11 08:19:09 +0300

    added TestBase.

--- adressbook-web-tests/src/test/java/stqa/pft/addressbook/ContactCreationTests.java
+++ adressbook-web-tests/src/test/java/stqa/pft/addressbook/ContactCreationTests.java
@@ -1,92 +1,17 @@
 package stqa.pft.addressbook;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import java.util.concurrent.TimeUnit;
-
-public class ContactCreationTests {
-    private WebDriver wd;
-
-    @BeforeClass(alwaysRun = true)
-    public void setUp() {
-        wd = new ChromeDriver();
-        wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        login("admin", "secret");
-    }
-
-    private void login(String userName, String password) {
-        wd.get("http://localhost/addressbook/");
-        wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys(userName);
-        wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys(password);
-        wd.findElement(By.xpath("//input[@value='Login']")).click();
-    }
+public class ContactCreationTests extends TestBase {
 
     @Test
-    public void testContactCreationTests() {
+    public void testContactCreation() {
         gotoContactPage();
         fillContactForm(new ContactData("Test1", "Test2", "st. Pushkina 104-20", "test@test.com"));
         submitContactCreation();
         gotoHomePage();
     }
 
-    private void gotoHomePage() {
-        wd.findElement(By.linkText("home page")).click();
-    }
-
-    private void submitContactCreation() {
-        wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
-    }
-
-    private void fillContactForm(ContactData contactData) {
-        wd.findElement(By.name("firstname")).click();
-        wd.findElement(By.name("firstname")).clear();
-        wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstName());
-        wd.findElement(By.name("lastname")).click();
-        wd.findElement(By.name("lastname")).clear();
-        wd.findElement(By.name("lastname")).sendKeys(contactData.getLastName());
-        wd.findElement(By.name("address")).click();
-        wd.findElement(By.name("address")).clear();
-        wd.findElement(By.name("address")).sendKeys(contactData.getAddress());
-        wd.findElement(By.name("email")).click();
-        wd.findElement(By.name("email")).clear();
-        wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
-    }
-
-    private void gotoContactPage() {
-        wd.findElement(By.linkText("add new")).click();
-    }
-
-    @AfterClass(alwaysRun = true)
-    public void tearDown() {
-        wd.quit();
-
-    }
-
-    private boolean isElementPresent(By by) {
-        try {
-            wd.findElement(by);
-            return true;
-        } catch (NoSuchElementException e) {
-            return false;
-        }
-    }
-
-    private boolean isAlertPresent() {
-        try {
-            wd.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
-    }
 
 }
