192a5f83e93890a5ba6dcde460bd5d1bd7d4fe91
StraniK3005 <StraniK.30.05@gmail.com>
2019-11-11 09:38:29 +0300

    created BaseHelper

--- adressbook-web-tests/src/test/java/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ adressbook-web-tests/src/test/java/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,8 +1,5 @@
 package stqa.pft.addressbook.appmanager;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -19,6 +16,7 @@ public class ApplicationManager {
     public void init() {
         wd = new ChromeDriver();
         wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        wd.get("http://localhost/addressbook/");
         sessionHelper = new SessionHelper(wd);
         groupHelper = new GroupHelper(wd);
         navigationHelper = new NavigationHelper(wd);
@@ -29,24 +27,6 @@ public class ApplicationManager {
 
     public void stop() {
         wd.quit();
-    }
-
-    public boolean isElementPresent(By by) {
-        try {
-            wd.findElement(by);
-            return true;
-        } catch (NoSuchElementException e) {
-            return false;
-        }
-    }
-
-    public boolean isAlertPresent() {
-        try {
-            wd.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
     }
 
     public GroupHelper getGroupHelper() {
